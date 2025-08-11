c775519e1611aa4e3dd11d4447d84866f830d95a
Asalul Musaffa <133022119+Asalulzy@users.noreply.github.com>
2025-08-11 10:10:26 +0700

    Update App.py

--- App.py
+++ App.py
@@ -80,7 +80,7 @@ def input_page():
     # Membuat 5 kolom dengan kolom tengah lebih lebar untuk logo
     col1, col2, col3, col4, col5, col6, col7,col8,col9 = st.columns([1,1,1,1,1.2,1,1,1,1])
     with col5:
-        st.image("LOgo.PNG", width=200)
+        st.image("assets/LOgo.png", width=200)
     
     st.markdown("<h1 style='text-align: center; color: #004b8d;'>Selamat Datang</h1>", unsafe_allow_html=True)
 
@@ -104,7 +104,7 @@ def analysis_page():
     df = load_data(st.session_state['uploaded_file'])
 
     # Sidebar filter
-    st.sidebar.image("LOgo.PNG", use_column_width=True)
+    st.sidebar.image("assets/LOgo.png", use_column_width=True)
     st.sidebar.title("🔍 Filter Data")
 
     # Filter Provinsi
